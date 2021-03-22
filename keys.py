801d97dfceef010e01da95074ec5b30a7009b59a
Kavi Gupta <kavi@berkeley.edu>
2021-03-21 22:11:44 -0700

    fix store

--- keys.py
+++ keys.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import hashlib
 
 
@@ -6,14 +7,19 @@ def key_hash(key):
     return hashlib.sha256(key.encode("utf-8")).hexdigest()
 
 
-def add_key():
+def add_key(name):
     entropy = os.urandom(256)
     key = hashlib.sha256(entropy).hexdigest()
     h = key_hash(key)
-    print(f"Your key: {key}. Save the key since it will not be displayed again")
+    print(f"Your key: '{key}'. Save the key since it will not be displayed again")
     with open("valid_key_hashes.txt", "a") as f:
-        f.write(h + "\n")
+        f.write(f"{h}: {name}\n")
+
+
+def all_hashes():
+    with open("valid_key_hashes.txt") as f:
+        return set(x.split(":")[0].strip() for x in f)
 
 
 if __name__ == "__main__":
-    add_key()
+    add_key(sys.argv[1])
--- store.py
+++ store.py
@@ -1,18 +1,48 @@
 import os
-import shelve
-from google.cloud import storage
+from itertools import count
 
-from keys import key_hash
+from keys import all_hashes
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("secret.json")
+default_app = firebase_admin.initialize_app(
+    cred, {"databaseURL": "https://mineral-subject-307700-default-rtdb.firebaseio.com/"}
+)
+
+
+class Store:
+    def __init__(self, reference):
+        self.reference = reference
+
+    def get(self, key):
+        reference = self.reference.child(key)
+        length = reference.child("length").get()
+        if length is None:
+            return None
+        result = []
+        for i in range(length):
+            value = reference.child(str(i)).get()
+            result.append(value)
+        return "".join(result)
+
+    def put(self, key, value):
+        reference = self.reference.child(key)
+        reference.delete()
+        reference.child("length").set(1)
+        reference.child("0").set(value)
+
+    def append(self, key, value):
+        reference = self.reference.child(key)
+        length = reference.child("length").get()
+        reference.child("length").set(length + 1)
+        reference.child(str(length)).set(value)
 
 
 def store(key):
-    with open("valid_key_hashes.txt") as f:
-        hashes = [x.strip() for x in set(f)]
     h = key_hash(key)
-    if h not in hashes:
+    if h not in all_hashes():
         return None
-
-    # Enable Cloud Storage
-    client = storage.Client()
-    # Reference an existing bucket.
-    return client.get_bucket(h)
+    return Store(db.reference("/").child(h))
